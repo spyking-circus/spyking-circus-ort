f7e24e0ac40c80c6212d33217e1c9d695befb615
BaptisteLefebvre <baptiste.lefebvre@ens.fr>
2017-03-07 11:05:05 +0100

    Remove all files in .gitignore

--- tests/test_acq_client.py
+++
@@ -1,9 +0,0 @@
-import circusort
-
-
-
-config = circusort.io.load_configuration()
-interface = config.acquisition.interface
-port = config.acquisition.port
-
-circusort.acq.spawn_client(interface, port)
