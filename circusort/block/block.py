42c75fcb5c5b71beb5ec2facbf0469fa98e5cd71
yger <pierre.yger@gmail.com>
2017-03-28 16:59:44 +0200

    Creation of a block

--- circusort/block/block.py
+++ circusort/block/block.py
@@ -7,72 +7,69 @@ from circusort.base.endpoint import Endpoint
 from circusort.base import utils
 
 
-# class Block(threading.Thread):
-#     '''TODO add docstring'''
+class Block(threading.Thread):
+    '''TODO add docstring'''
 
-#     def __init__(self, log_address=None, log_level=logging.INFO):
+    def __init__(self, name, log_address=None, log_level=logging.INFO):
 
-#         threading.Thread.__init__(self)
+        threading.Thread.__init__(self)
 
-#         self.log_address = log_address
-#         self.log_level = log_level
+        self.log_address = log_address
+        self.log_level = log_level
 
-#         self.name = "Computer 1"
-#         if self.log_address is None:
-#             raise NotImplementedError("no logger address")
-#         self.log = utils.get_log(self.log_address, name=__name__, self.log_level)
+        self.name = "Computer 1"
+        if self.log_address is None:
+            raise NotImplementedError("no logger address")
+        self.log = utils.get_log(self.log_address, name=__name__, log_level=self.log_level)
 
-#         self.inputs  =  {}
-#         self.outputs =  {}
-#         self.running  = False
+        self.inputs  =  {}
+        self.outputs =  {}
+        self.running  = False
 
-#         self.log.info("computer 1 created")
+        self.log.info("computer 1 created")
 
-#     def initialize(self):
-#         '''TODO add docstring'''
+    def initialize(self, **kwargs):
+        '''TODO add docstring'''
 
-#         self.log.debug("initialization")
+        self.log.debug("initialization")
 
-#         # Bind socket for input data
-#         transport = 'tcp'
-#         host = '127.0.0.1'
-#         port = '*'
-#         endpoint = '{h}:{p}'.format(h=host, p=port)
-#         address = '{t}://{e}'.format(t=transport, e=endpoint)
-#         self.input.socket = self.context.socket(zmq.PAIR)
-#         # self.input.socket.setsockopt(zmq.RCVTIMEO, 10000)
-#         self.input.socket.bind(address)
-#         self.input.addr = self.input.socket.getsockopt(zmq.LAST_ENDPOINT)
+        # Bind socket for input data
+        transport = 'tcp'
+        host = '127.0.0.1'
+        port = '*'
+        endpoint = '{h}:{p}'.format(h=host, p=port)
+        address = '{t}://{e}'.format(t=transport, e=endpoint)
+        self.input.socket = self.context.socket(zmq.PAIR)
+        # self.input.socket.setsockopt(zmq.RCVTIMEO, 10000)
+        self.input.socket.bind(address)
+        self.input.addr = self.input.socket.getsockopt(zmq.LAST_ENDPOINT)
 
-#         return
+        return
 
-#     def connect(self):
-#         '''TODO add docstring'''
+    def connect(self):
+        '''TODO add docstring'''
 
-#         self.log.debug("connection")
+        self.log.debug("connection")
 
-#         self.output.socket = self.context.socket(zmq.PAIR)
-#         self.output.socket.connect(self.output.addr)
+        self.output.socket = self.context.socket(zmq.PAIR)
+        self.output.socket.connect(self.output.addr)
 
-#         return
+        return
 
-#     def configure(self):
-#         '''TODO add docstring'''
+    def configure(self, **kwargs):
+        '''TODO add docstring'''
 
-#         self.log.debug("configuration")
+        self.log.debug("configuration")
 
-#         self.output.dtype = self.input.dtype
-#         self.output.shape = self.input.shape
+        return self._configure(**kwags)
 
-#         return
+    def run(self):
+        '''TODO add dosctring'''
 
-#     def run(self):
-#         '''TODO add dosctring'''
+        self.log.debug("run")
+        self.running = True
 
-#         self.log.debug("run")
-#         self.running = True
-
-#         while self.running:
-#             self._run()
+        while self.running:
+            self._run()
 
 
