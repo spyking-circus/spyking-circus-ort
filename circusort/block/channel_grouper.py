e88a3c215586345a413870dab6a9c959f02728f7
yger <pierre.yger@gmail.com>
2017-04-05 17:31:48 +0200

    And add the channel grouper counterpart

---
+++ circusort/block/channel_grouper.py
@@ -0,0 +1,51 @@
+from .block import Block
+import numpy
+
+class Channel_grouper(Block):
+    '''TODO add docstring'''
+
+    name   = "Channel Grouper"
+
+    params = {'nb_groups' : 1}
+
+    def __init__(self, **kwargs):
+        Block.__init__(self, **kwargs)
+        self.add_output('data')
+        for i in xrange(self.nb_groups):
+            self.add_input('data_%d' %i)    
+        
+    @property
+    def nb_channels(self):
+        shape = 0
+        for i in xrange(self.nb_groups):
+            shape += self.get_input('data_%d' %i).shape[0]
+        return shape
+
+    @property
+    def nb_samples(self):
+        return self.get_input('data_0').shape[1]
+
+    @property
+    def dtype(self):
+        return self.get_input('data_0').dtype
+
+
+    def _initialize(self):
+        return
+
+    def _guess_output_endpoints(self):
+        try:
+            self.output.configure(dtype=self.dtype, shape=(self.nb_channels, self.nb_samples))
+            self.result = numpy.zeros((self.nb_channels, self.nb_samples), dtype=self.dtype)
+        except Exception:
+            self.log.debug('Not all input connections have been established!')
+
+
+    def _process(self):
+        
+        for i in xrange(self.nb_groups):
+            batch = self.get_input('data_%i' %i).receive()
+            self.result[i::self.nb_groups] = batch
+
+        self.output.send(self.result.flatten())
+        return
--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -15,25 +15,22 @@ manager   = director.create_manager(host=host, log_level=logging.INFO)
 
 noise      = manager.create_block('noise_generator')
 dispatcher = manager.create_block('channel_dispatcher', nb_groups=nb_groups)
-filters = [manager.create_block('filter', name='Filter %d' %i) for i in xrange(nb_groups)]
+filters    = [manager.create_block('filter', name='Filter %d' %i) for i in xrange(nb_groups)]
+regrouper  = manager.create_block('channel_grouper', nb_groups=nb_groups) 
 
 manager.initialize()
-
 
 manager.connect(noise.output, dispatcher.input)
 
 
-
 for i in xrange(nb_groups):
     manager.connect(dispatcher.get_output('data_%d' %i), filters[i].input)
-
-
-
+    manager.connect(filters[i].output, regrouper.get_input('data_%d' %i))
 
 
 manager.start()
 
 # TODO save computational times to file
-director.sleep(duration=2.0)
+director.sleep(duration=10.0)
 
 director.stop()
