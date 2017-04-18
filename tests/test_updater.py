0b869eb68a8d1953e9edd0521cfaac6e2f95e95d
yger <pierre.yger@gmail.com>
2017-04-18 19:39:53 +0200

    WIP on the template updater

---
+++ tests/test_updater.py
@@ -0,0 +1,40 @@
+# Test to measure the computational efficiency of two operations in one block
+# associated to one manager.
+
+import circusort
+import settings
+import logging
+from circusort.io.utils import generate_fake_probe
+
+
+host = '127.0.0.1' # to run the test locally
+
+director      = circusort.create_director(host=host)
+manager       = director.create_manager(host=host)
+manager2      = director.create_manager(host=host)
+
+nb_channels   = 10
+probe_file    = generate_fake_probe(nb_channels)
+
+noise         = manager.create_block('fake_spike_generator', nb_channels=nb_channels)
+filter        = manager.create_block('filter')
+whitening     = manager.create_block('whitening')
+mad_estimator = manager.create_block('mad_estimator')
+peak_detector = manager.create_block('peak_detector', threshold=5)
+pca           = manager.create_block('pca', nb_waveforms=5000)
+cluster       = manager2.create_block('density_clustering', probe=probe_file, nb_waveforms=1000)
+updater       = manager2.create_block('template_updater', probe=probe_file, log_level=logging.DEBUG)
+
+director.initialize()
+
+director.connect(noise.output, filter.input)
+director.connect(filter.output, whitening.input)
+director.connect(whitening.output, [mad_estimator.input, peak_detector.get_input('data'), cluster.get_input('data'), pca.get_input('data'), updater.get_input('data')])
+director.connect(mad_estimator.output, [peak_detector.get_input('mads'), cluster.get_input('mads')])
+director.connect(peak_detector.get_output('peaks'), [pca.get_input('peaks'), cluster.get_input('peaks')])
+director.connect(pca.get_output('pcs'), cluster.get_input('pcs'))
+director.connect(cluster.get_output('templates'), updater.get_input('templates'))
+
+director.start()
+director.sleep(duration=60.0)
+director.stop()
