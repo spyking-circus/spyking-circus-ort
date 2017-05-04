ef316107e4b06b83eb099d819e7a7a68f953f02a
yger <pierre.yger@gmail.com>
2017-05-04 15:20:54 +0200

    WIP online clustering

--- circusort/utils/clustering.py
+++ circusort/utils/clustering.py
@@ -1,174 +1,225 @@
 import scipy.optimize, numpy, pylab, scipy.spatial.distance, scipy.stats
 import warnings
+import logging
 warnings.filterwarnings("ignore")
 
+class MacroCluster(object):
+
+    def __init__(self, id, data, creation_time=0):
+
+        self.id             = id
+        self.density        = len(data)
+        self.sum_centers    = numpy.sum(data, 0)
+        self.sum_centers_sq = numpy.sum(data**2, 0)
+        self.creation_time  = creation_time
+        self.last_update    = creation_time
+        self.label          = None
+
+    def set_label(self, label):
+        assert label in ['sparse', 'dense']
+        self.label = label    
+
+    @property
+    def is_sparse(self):
+        return self.label == 'sparse'
+
+    @property
+    def is_dense(self):
+        return self.label == 'dense'
+
+    def add_and_update(self, data, time, decay_factor):
+        factor              = 2**(-decay_factor*(time - self.last_update))
+        self.density        = factor*self.density + 1.
+        self.sum_centers    = factor*self.sum_centers + data
+        self.sum_centers_sq = factor*self.sum_centers_sq + data**2
+        self.last_update    = time
+
+    def remove(self, data):
+        self.density        -= 1
+        self.sum_centers    -= data
+        self.sum_centers_sq -= data**2
+
+    @property
+    def center(self):
+        return self.sum_centers/self.density
+
+    @property
+    def sigma(self):
+        return numpy.sqrt(numpy.linalg.norm(self.sum_centers_sq/self.density - self.center**2))
+
+    def get_z_score(self, data, sigma):
+        return numpy.linalg.norm(self.center - data)/sigma
 
 class OnlineManager(object):
 
-    def __init__(self, decay_factor=0.35, mu=4, epsilon=0.5, theta=-numpy.log(0.001)):
+    def __init__(self, decay_factor=0.35, mu=4, epsilon=0.5, theta=-numpy.log(0.001), logger=None, name=None):
 
-        self.densities        = {}
-        self.last_updates     = {}   
-        self.summed_centers   = {}
-        self.summed_centers_2 = {}
-        self.creation_times   = {}
-        self.clusters         = {'sparse' : [], 'dense' : []}
-    
-    def initialize(self, data, labels):
-        for count, i in enumerate(numpy.unique(clusters[clusters > -1])):
-            self.densities[count]        = numpy.sum(clusters == i)
-            self.last_updates[count]     = 0
-            self.creation_times[count]   = 0
-            self.summed_centers[count]   = numpy.sum(data[clusters == i], 0)
-            self.summed_centers_2[count] = numpy.sum(data[clusters == i]**2, 0)
-
-        for cluster in self.densities.keys():
-            if self.densities[cluster] >= self.D_thred:
-                self.clusters['dense']  += [cluster]
-            else:
-                self.clusters['sparse'] += [cluster]
-
-    def check_center(self):
-        pass
-
-    def _online_update(self, time):
-        self.D_thred  = mu/(len(self.densities)*(1-2**(-self.decay_factor)))
-        #if D_thred > 1:
-        #    time_gap = (1/(decay_factor))*numpy.log(D_thred/(D_thred - 1))
-        #   #time_gap = numpy.log(mu/(mu - len(densities)*(1-2**-decay_factor)))/numpy.log(decay_factor)
-        #else:
-            #time_gap = 1
-        self.time_gap = 100
-
-
-        # Update of D_thred and time_gap to know when we need to clean clusters, and where
-        # is the threshold between sparse and dense clusters
-
-            
-        #for cluster in (sparse_clusters + dense_clusters):
-        #    densities[cluster]        *= 2**(-decay_factor)
-        #    summed_centers[cluster]   *= 2**(-decay_factor)
-        #    summed_centers_2[cluster] *= 2**(-decay_factor)
-
-        # First, we collect all dense cluster centers
-        dense_centers = numpy.zeros((0, nb_dimensions))
-        dense_radius  = []
-        for cluster in self.dense_clusters:
-            dense_centers = numpy.vstack((dense_centers, numpy.array(self.summed_centers[cluster]/self.densities[cluster])))
-            x_src         = self.summed_centers_2[cluster]/self.densities[cluster]
-            x_tgt         = (self.summed_centers[cluster]/self.densities[cluster])**2
-            #dense_radius += [numpy.sqrt(numpy.max([0, numpy.linalg.norm(x_src) - numpy.linalg.norm(x_tgt)]))]   #[1.8*numpy.sqrt(numpy.max(numpy.maximum(x_src - x_tgt, numpy.zeros(nb_dimensions))))] #[scipy.spatial.distance.cdist(x_src.reshape(1, nb_dimensions), x_tgt.reshape(1, nb_dimensions))[0, 0]]
-            dense_radius += [1.8*numpy.sqrt(numpy.max(numpy.maximum(x_src - x_tgt, numpy.zeros(nb_dimensions))))]
-
-        # We check if a point is next to a dense cluster, up to epsilon [TO DEFINE]
-        to_be_merged = False
-        if len(self.dense_centers) > 0:
-            new_dist = scipy.spatial.distance.cdist(new_data, self.dense_centers, 'euclidean')[0]
-            dist_min = numpy.min(new_dist)
-            dist_idx = self.dense_clusters[numpy.argmin(new_dist)]
-
-            factor     = 2**(-self.decay_factor*(time - self.last_updates[dist_idx]))
-            x_src      = (self.summed_centers_2[dist_idx]*factor +  new_data[0]**2)/(self.densities[dist_idx]*factor + 1)
-            x_tgt      = ((self.summed_centers[dist_idx]*factor + new_data[0])/(self.densities[dist_idx]*factor + 1))**2
-            #new_radius = numpy.sqrt(numpy.max([0, numpy.linalg.norm(x_src) - numpy.linalg.norm(x_tgt)]))   #1.8*numpy.sqrt(numpy.max(numpy.maximum(x_src - x_tgt, numpy.zeros(nb_dimensions))))
-            new_radius = 1.8*numpy.sqrt(numpy.max(numpy.maximum(x_src - x_tgt, numpy.zeros(nb_dimensions))))
-
-            to_be_merged = new_radius < self.epsilon #dense_radius[numpy.argmin(new_dist)]
-
-        if to_be_merged:
-            # If yes, we assign it to the dense cluster, and update the corresponding density and center
-            if verbose:
-                print "Data assigned to dense cluster", dist_idx 
-            self.densities[dist_idx]        = factor*self.densities[dist_idx] + 1
-            self.summed_centers[dist_idx]   = factor*self.summed_centers[dist_idx] + new_data[0]
-            self.summed_centers_2[dist_idx] = factor*self.summed_centers_2[dist_idx] + new_data[0]**2
-            #densities[dist_idx]        += 1
-            #summed_centers[dist_idx]   += new_data[0]
-            #summed_centers_2[dist_idx] += new_data[0]**2
-            self.last_updates[dist_idx]     = time
+        if name is None:
+            self.name = "OnlineManager"
         else:
-            # If no, we search in the sparse clusters
-            sparse_centers = numpy.zeros((0, nb_dimensions))
-            sparse_radius  = []
-            for cluster in self.sparse_clusters:
-                sparse_centers = numpy.vstack((sparse_centers, numpy.array(self.summed_centers[cluster]/self.densities[cluster])))
-                x_src          = self.summed_centers_2[cluster]/self.densities[cluster]
-                x_tgt          = (self.summed_centers[cluster]/self.densities[cluster])**2
-                #sparse_radius += [numpy.sqrt(numpy.max([0, numpy.linalg.norm(x_src) - numpy.linalg.norm(x_tgt)]))]   #[1.8*numpy.sqrt(numpy.max(numpy.maximum(x_src - x_tgt, numpy.zeros(nb_dimensions))))] #[scipy.spatial.distance.cdist(x_src.reshape(1, nb_dimensions), x_tgt.reshape(1, nb_dimensions))[0, 0]]
-                sparse_radius  += [1.8*numpy.sqrt(numpy.max(numpy.maximum(x_src - x_tgt, numpy.zeros(nb_dimensions))))]
+            self.name = name
+        self.clusters         = {}
+        self.decay_factor     = decay_factor
+        self.mu               = mu
+        self.epsilon          = epsilon
+        self.theta            = theta
+        self.is_ready         = False
+        if logger is None:
+            self.log = logging.getLogger(__name__)
+        else:
+            self.log = logger
+    
+    @property
+    def D_threshold(self):
+        return self.mu/(self.nb_clusters*(1 - 2**(-self.decay_factor)))
 
-            to_be_merged = False
-            if len(self.sparse_clusters) > 0:
-                new_dist = scipy.spatial.distance.cdist(new_data, sparse_centers, 'euclidean')[0]
-                dist_min = numpy.min(new_dist)
-                dist_idx = self.sparse_clusters[numpy.argmin(new_dist)]
+    @property
+    def time_gap(self):
+        if self.D_threshold > 1:
+            return numpy.ceil((1/(self.decay_factor))*numpy.log(self.D_threshold/(self.D_threshold - 1)))
+        else:
+            return 100
+
+    def initialize(self, time, data, labels):
+
+        mask               = labels > -1
+        self.nb_dimensions = data.shape[1]
+
+        for count, i in enumerate(numpy.unique(labels[mask])):
+
+            indices = numpy.where(labels == i)[0]
+            self.clusters[count] = MacroCluster(count, data[indices], creation_time=time)
+
+        for cluster in self.clusters.values():
+            if cluster.density >= self.D_threshold:
+                cluster.set_label('dense')
+            else:
+                cluster.set_label('sparse')
+
+        self.is_ready = True
+
+    @property
+    def nb_sparse(self):
+        return len(self.sparse_clusters)
+
+    @property
+    def nb_dense(self):
+        return len(self.dense_clusters)
+
+    @property
+    def nb_clusters(self):
+        return len(self.clusters)
+
+    @property
+    def dense_clusters(self):
+        return [i for i in self.clusters.values() if i.is_dense]
+
+    @property
+    def sparse_clusters(self):
+        return [i for i in self.clusters.values() if i.is_sparse]
+
+    def _get_id(self):
+        return numpy.max(self.clusters.keys()) + 1
+
+    def _get_clusters(self, cluster_type):
+        if cluster_type == 'sparse':
+            return self.sparse_clusters
+        elif cluster_type == 'dense':
+            return self.dense_clusters
+
+    def _get_centers(self, cluster_type='dense'):
+        centers = numpy.zeros((0, self.nb_dimensions), dtype=numpy.float32)
+        for cluster in self._get_clusters(cluster_type):
+            centers = numpy.vstack((centers, [cluster.center]))
+        return centers
+
+    def _merged_into(self, data, cluster_type):
                 
-                factor     = 2**(-self.decay_factor*(time - self.last_updates[dist_idx]))
-                x_src      = (self.summed_centers_2[dist_idx]*factor +  new_data[0]**2)/(self.densities[dist_idx]*factor + 1)
-                x_tgt      = ((self.summed_centers[dist_idx]*factor + new_data[0])/(self.densities[dist_idx]*factor + 1))**2
-                #new_radius = numpy.sqrt(numpy.max([0, numpy.linalg.norm(x_src) - numpy.linalg.norm(x_tgt)])) #1.8*numpy.sqrt(numpy.max(numpy.maximum(x_src - x_tgt, numpy.zeros(nb_dimensions))))
-                new_radius = 1.8*numpy.sqrt(numpy.max(numpy.maximum(x_src - x_tgt, numpy.zeros(nb_dimensions))))
+        clusters     = self._get_clusters(cluster_type)
+        to_be_merged = False
 
-                to_be_merged = new_radius < self.epsilon #dense_radius[numpy.argmin(new_dist)]
+        if len(clusters) > 0:
 
-                #to_be_merged = dist_min < epsilon #sparse_radius[numpy.argmin(new_dist)]
+            centers  = self._get_centers(cluster_type)
+            new_dist = scipy.spatial.distance.cdist(data, centers, 'euclidean')[0]
+            dist_min = numpy.min(new_dist)
+            cluster  = clusters[numpy.argmin(new_dist)]
 
+            cluster.add_and_update(data[0], self.time, self.decay_factor)
+            sigma = cluster.sigma
+            
+            if sigma == 0:
+                sigma = self._estimate_sigma()
+            
+            to_be_merged = cluster.get_z_score(data[0], sigma) <= self.epsilon
 
             if to_be_merged:
-                if verbose:
-                    print "Data assigned to sparse cluster", dist_idx 
-                self.densities[dist_idx]        = factor*self.densities[dist_idx] + 1
-                self.summed_centers[dist_idx]   = factor*self.summed_centers[dist_idx] + new_data[0]
-                self.summed_centers_2[dist_idx] = factor*self.summed_centers_2[dist_idx] + new_data[0]**2
-                #densities[dist_idx]        += 1
-                #summed_centers[dist_idx]   += new_data[0]
-                #summed_centers_2[dist_idx] += new_data[0]**2
-                self.last_updates[dist_idx]     = time
-                if self.densities[dist_idx] >= self.D_thred:
-                    self.sparse_clusters.remove(dist_idx)
-                    self.dense_clusters += [dist_idx]
+                if cluster.density >= self.D_threshold:
+                    cluster.set_label('dense')
             else:
-                new_idx = numpy.max(densities.keys()) + 1
-                if verbose:
-                   print "Creating a new cluster", new_idx
-                self.densities[new_idx]        = 1.
-                self.creation_times[new_idx]   = time
-                self.last_updates[new_idx]     = time
-                self.summed_centers[new_idx]   = new_data[0]
-                self.summed_centers_2[new_idx] = new_data[0]**2
-                self.sparse_clusters          += [new_idx]
+                cluster.remove(data[0])
 
-        if numpy.mod(time, self.time_gap) < 1:
+        return to_be_merged
 
-    #        for cluster in (sparse_clusters + dense_clusters):
-    #            densities[cluster]        *= 2**(-decay_factor*(time - last_updates[cluster]))
-    #            summed_centers[cluster]   *= 2**(-decay_factor*(time - last_updates[cluster]))
-    #            summed_centers_2[cluster] *= 2**(-decay_factor*(time - last_updates[cluster]))
 
-            if verbose:
-                print "Time gap reached, time to clean clusters..."
+    def _estimate_sigma(self):
+        sigma = 0
+        count = 0
+        for cluster in self.clusters.values():
+            if cluster.sigma > 0:
+                sigma += cluster.sigma
+                count += 1
+        return sigma/count
 
-            for cluster in self.dense_clusters:
-                if self.densities[cluster] < self.D_thred:
-                    self.dense_clusters.remove(cluster)
-                    self.sparse_clusters += [cluster]
+    def _prune(self):
+        self.log.debug("Time gap reached, time to clean clusters...")
 
-            for cluster in self.sparse_clusters:
+        for cluster in self.dense_clusters:
+            if cluster.density < self.D_threshold:
+                cluster.set_label('sparse')
 
-                T_0     = self.creation_times[cluster]
-                if T_0 < time and T_0 > 0:
-                    zeta    = (2**(-self.decay_factor*(time - T_0 + time_gap)) - 1)/(2**(-self.decay_factor*self.time_gap) - 1)
-                    delta_t = self.theta*(self.last_updates[cluster] - T_0)/self.densities[cluster] 
+        for cluster in self.sparse_clusters:
 
-                    if self.densities[cluster] < zeta or ((time - self.last_updates[cluster]) > delta_t):
-                        if verbose:
-                            print "Removing sparse cluster", cluster
-                        self.densities.pop(cluster)
-                        self.last_updates.pop(cluster)
-                        self.summed_centers.pop(cluster)
-                        self.summed_centers_2.pop(cluster)
-                        self.creation_times.pop(cluster)
-                        self.sparse_clusters.remove(cluster)
+            T_0     = cluster.creation_time
+
+            if T_0 < self.time and T_0 > 0:
+
+                zeta    = (2**(-self.decay_factor*(self.time - T_0 + self.time_gap)) - 1)/(2**(-self.decay_factor*self.time_gap) - 1)
+                delta_t = self.theta*(cluster.last_update - T_0)/cluster.density
+
+                if cluster.density < zeta or ((time - cluster.last_update) > delta_t):
+                    self.log.debug("Removing sparse cluster %d" %cluster.id)
+
+                    self.clusters.pop(cluster.id)
+
+    def update(self, time, new_data):
+        
+        self.time = time
+        self.log.debug("Processing time {t} with {s} sparse/ {d} dense".format(t=time, s=self.nb_sparse, d=self.nb_dense)) 
+        if self._merged_into(new_data, 'dense'):
+            self.log.debug("We merged the time point", self.time, 'into a dense cluster')
+        else:
+            if self._merged_into(new_data, 'sparse'):
+                self.log.debug("We merged the time point", self.time, 'into a sparse cluster')
+            else:
+                self.log.debug("We can not merged time point", self.time, "so creating a new sparse cluster")
+                new_id      = self._get_id()
+                new_cluster = MacroCluster(new_id, new_data, creation_time=self.time)
+                new_cluster.set_label('sparse')
+                self.clusters[new_id] = new_cluster
+
+        if numpy.mod(self.time, self.time_gap) == 0:
+            self._prune()
+
+
+    def cluster(self):
+        final_data = numpy.zeros((self.nb_dense, self.nb_dimensions))
+        for count, cluster in enumerate(self.dense_clusters):
+            final_data[count] = cluster.center
+
+        rhos, dist        = rho_estimation(final_data)
+        rhos              = -rhos + rhos.max() 
+        clusters, r, d, c = clustering(rhos, dist, n_min=None)
 
 
 def distancematrix(data):
