e616f222e5ed496abb2947611dd8b6f0b59962ab
balefebvre <baptiste.lefebvre@ens.fr>
2017-10-20 11:59:15 +0200

    Add I/O function to load peak file

--- circusort/io/__init__.py
+++ circusort/io/__init__.py
@@ -2,6 +2,6 @@ from .base import isdata
 from .configure import load_configuration
 from . import configure
 from . import generate
-from .load import load
+from .load import load, load_peaks
 from .configure import Configuration
 from .probe import Probe
--- circusort/io/load.py
+++ circusort/io/load.py
@@ -5,14 +5,14 @@ import ConfigParser as cp
 from .utils import *
 
 
-
 def load(path):
     data = raw_binary(path)
     return data
 
 
 class RawBinary(object):
-    '''TODO add doc...'''
+    """Raw binary"""
+    # TODO complete docstring.
 
     def __init__(self, path, dtype, length, nb_channels, sampling_rate):
         self.path = path
@@ -28,7 +28,6 @@ class RawBinary(object):
         return data
 
 
-
 def raw_binary(path):
     path = os.path.expanduser(path)
     header_path = get_header_path(path)
@@ -41,3 +40,50 @@ def raw_binary(path):
     raw = RawBinary(path, dtype, length, nb_channels, sampling_rate)
     data = raw.load()
     return data
+
+
+def load_peaks(path):
+    """Load peaks
+
+    Arguments:
+        path: string
+
+    """
+    # TODO complete docstring.
+
+    return Peaks(path)
+
+
+class Peaks(object):
+    """Peaks"""
+    # TODO complete docstring.
+
+    def __init__(self, path):
+
+        self.path = os.path.expanduser(path)
+        self.data = np.fromfile(self.path, dtype=np.int32)
+
+    @property
+    def electrodes(self):
+
+        return self.data[0::2]
+
+    @property
+    def time_steps(self):
+
+        return self.data[1::2]
+
+    def get_time_steps(self, selection=None):
+
+        if selection is None:
+            ans = self.time_steps
+        elif isinstance(selection, int):
+            mask = np.array([e == selection for e in self.electrodes], dtype=np.bool)
+            ans = self.time_steps[mask]
+        elif isinstance(selection, list):
+            mask = np.array([e in selection for e in self.electrodes], dtype=np.bool)
+            ans = self.time_steps[mask]
+        else:
+            raise NotImplementedError()
+
+        return ans
