afa43ffdb596155fc83262b0619722138936a2c9
balefebvre <baptiste.lefebvre@ens.fr>
2017-10-20 11:35:43 +0200

    Add threshold_factor parameter & Clean & Document

--- circusort/block/peak_detector.py
+++ circusort/block/peak_detector.py
@@ -1,17 +1,35 @@
 from .block import Block
-import numpy
+import numpy as np
 
 
 class Peak_detector(Block):
-    """Peak detector block"""
+    """Peak detector block
+
+    Attributes:
+        threshold_factor
+        sign_peaks
+        spike_width
+        sampling_rate
+        safety_time
+
+    Inputs:
+        data
+        mads
+
+    Outputs:
+        peaks
+        dict
+
+    """
     # TODO complete docstring.
 
     name = "Peak detector"
 
     params = {
+        'threshold_factor': 5.0,
         'sign_peaks': 'negative',
-        'spike_width': 5,
-        'sampling_rate': 20000.,
+        'spike_width': 5.0,  # ms
+        'sampling_rate': 20e+3,  # Hz
         'safety_time': 'auto',
     }
 
@@ -23,30 +41,35 @@ class Peak_detector(Block):
         self.add_input('data')
 
     def _initialize(self):
-        self.peaks = {'offset' : 0}
+
+        self.peaks = {'offset': 0}
         if self.sign_peaks == 'both':
             self.key_peaks = ['negative', 'positive']
         else:
             self.key_peaks = [self.sign_peaks]
-        self._spike_width_ = int(self.sampling_rate*self.spike_width*1e-3)
-        if numpy.mod(self._spike_width_, 2) == 0:
+        self._spike_width_ = int(self.sampling_rate * self.spike_width * 1e-3)
+        if np.mod(self._spike_width_, 2) == 0:
             self._spike_width_ += 1
-        self._width = self._spike_width_/2
+        self._width = self._spike_width_ / 2
         if self.safety_time == 'auto':
             self.safety_time = self._width
         else:
-            self.safety_time = max(1, int(self.sampling_rate*self.safety_time*1e-3))
+            self.safety_time = max(1, int(self.sampling_rate * self.safety_time * 1e-3))
+
         return
 
     @property
     def nb_channels(self):
+
         return self.inputs['data'].shape[1]
         
     @property
     def nb_samples(self):
+
         return self.inputs['data'].shape[0]
 
     def _guess_output_endpoints(self):
+
         return
 
     def _detect_peaks(self, x, mph=None, mpd=1, threshold=0, edge='rising', kpsh=False, valley=False):
@@ -55,15 +78,15 @@ class Peak_detector(Block):
             x = -x
         # find indices of all peaks
         dx = x[1:] - x[:-1]
-        ine, ire, ife = numpy.array([[], [], []], dtype=numpy.int32)
+        ine, ire, ife = np.array([[], [], []], dtype=np.int32)
         if not edge:
-            ine = numpy.where((numpy.hstack((dx, 0)) < 0) & (numpy.hstack((0, dx)) > 0))[0]
+            ine = np.where((np.hstack((dx, 0)) < 0) & (np.hstack((0, dx)) > 0))[0]
         else:
             if edge.lower() in ['rising', 'both']:
-                ire = numpy.where((numpy.hstack((dx, 0)) <= 0) & (numpy.hstack((0, dx)) > 0))[0]
+                ire = np.where((np.hstack((dx, 0)) <= 0) & (np.hstack((0, dx)) > 0))[0]
             if edge.lower() in ['falling', 'both']:
-                ife = numpy.where((numpy.hstack((dx, 0)) < 0) & (numpy.hstack((0, dx)) >= 0))[0]
-        ind = numpy.unique(numpy.hstack((ine, ire, ife)))
+                ife = np.where((np.hstack((dx, 0)) < 0) & (np.hstack((0, dx)) >= 0))[0]
+        ind = np.unique(np.hstack((ine, ire, ife)))
         # first and last values of x cannot be peaks
         if ind.size and ind[0] == 0:
             ind = ind[1:]
@@ -74,12 +97,12 @@ class Peak_detector(Block):
             ind = ind[x[ind] >= mph]
         # remove peaks - neighbors < threshold
         if ind.size and threshold > 0:
-            dx = numpy.min(numpy.vstack([x[ind]-x[ind-1], x[ind]-x[ind+1]]), axis=0)
-            ind = numpy.delete(ind, numpy.where(dx < threshold)[0])
+            dx = np.min(np.vstack([x[ind]-x[ind-1], x[ind]-x[ind+1]]), axis=0)
+            ind = np.delete(ind, np.where(dx < threshold)[0])
         # detect small peaks closer than minimum peak distance
         if ind.size and mpd > 1:
-            ind = ind[numpy.argsort(x[ind])][::-1]  # sort ind by peak height
-            idel = numpy.zeros(ind.size, dtype=numpy.bool)
+            ind = ind[np.argsort(x[ind])][::-1]  # sort ind by peak height
+            idel = np.zeros(ind.size, dtype=np.bool)
             for i in range(ind.size):
                 if not idel[i]:
                     # keep peaks with the same height if kpsh is True
@@ -87,7 +110,7 @@ class Peak_detector(Block):
                         & (x[ind[i]] > x[ind] if kpsh else True)
                     idel[i] = 0  # Keep current peak
             # remove the small peaks and sort back the indices by their occurrence
-            ind = numpy.sort(ind[~idel])
+            ind = np.sort(ind[~idel])
 
         return ind
 
@@ -104,16 +127,17 @@ class Peak_detector(Block):
             for key in self.key_peaks:
                 self.peaks[key] = {}
                 for i in xrange(self.nb_channels):
+                    threshold = self.threshold_factor * thresholds[i]
                     if key == 'negative':
-                        data = self._detect_peaks(batch[:, i],  thresholds[i], valley=True, mpd=self.safety_time)
+                        data = self._detect_peaks(batch[:, i],  threshold, valley=True, mpd=self.safety_time)
                         if len(data) > 0:
                             self.peaks[key][i] = data
                     elif key == 'positive':
-                        data = self._detect_peaks(batch[:, i],  thresholds[i], valley=False, mpd=self.safety_time)
+                        data = self._detect_peaks(batch[:, i],  threshold, valley=False, mpd=self.safety_time)
                         if len(data) > 0:
                             self.peaks[key][i] = data
 
-            self.peaks['offset'] = self.counter*self.nb_samples
+            self.peaks['offset'] = self.counter * self.nb_samples
             self.outputs['peaks'].send(self.peaks)
 
         return
