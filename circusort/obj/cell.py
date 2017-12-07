0bbd04ee9b000ac419bb76f673514d0f848617e2
balefebvre <baptiste.lefebvre@ens.fr>
2017-12-07 16:21:19 +0100

    Adding skeleton for cell object

--- circusort/obj/cell.py
+++ circusort/obj/cell.py
@@ -1,6 +1,31 @@
 class Cell(object):
     # TODO add docstring.
 
-    def __init__(self):
+    def __init__(self, template, train):
 
-        raise NotImplementedError()
+        self.template = template
+        self.train = train
+
+        # TODO correct the following lines.
+        self.x = lambda _: 0.0
+        self.y = lambda _: 0.0
+        self.z = lambda _: 0.0
+        self.e = lambda _, probe: 0
+        self.r = lambda _: 0.0
+
+        raise NotImplementedError()  # TODO complete.
+
+    def get_waveform(self):
+        # TODO add docstring.
+
+        raise NotImplementedError()  # TODO complete.
+
+    def get_waveforms(self):
+        # TODO add docstring.
+
+        raise NotImplementedError()  # TODO complete.
+
+    def generate_spike_trains(self):
+        # TODO add docstring.
+
+        raise NotImplementedError()  # TODO complete.
