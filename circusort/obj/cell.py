9a15ae0a1b9115701715d1968111d75dfeac9996
balefebvre <baptiste.lefebvre@ens.fr>
2017-12-07 19:13:36 +0100

    Cleaning skeleton for cell object

--- circusort/obj/cell.py
+++ circusort/obj/cell.py
@@ -6,26 +6,6 @@ class Cell(object):
         self.template = template
         self.train = train
 
-        # TODO correct the following lines.
-        self.x = lambda _: 0.0
-        self.y = lambda _: 0.0
-        self.z = lambda _: 0.0
-        self.e = lambda _, probe: 0
-        self.r = lambda _: 0.0
+        # TODO complete.
 
-        raise NotImplementedError()  # TODO complete.
-
-    def get_waveform(self):
-        # TODO add docstring.
-
-        raise NotImplementedError()  # TODO complete.
-
-    def get_waveforms(self):
-        # TODO add docstring.
-
-        raise NotImplementedError()  # TODO complete.
-
-    def generate_spike_trains(self):
-        # TODO add docstring.
-
-        raise NotImplementedError()  # TODO complete.
+    # TODO complete.
