70f9156f45c6376dfecba7d989e91ff07ed1c9e5
balefebvre <baptiste.lefebvre@ens.fr>
2017-12-07 14:59:06 +0100

    Fix imports in io/cell

--- circusort/io/cell.py
+++ circusort/io/cell.py
@@ -1,6 +1,7 @@
 import os
 
-from circusort import io
+from .template import list_templates, load_template
+from .trains import list_trains, load_train
 
 
 def load_cells(working_directory=None):
@@ -14,18 +15,18 @@ def load_cells(working_directory=None):
     generation_directory = os.path.join(working_directory, "generation")
 
     template_directory = os.path.join(generation_directory, "template")
-    template_paths = io.list_templates(template_directory)
+    template_paths = list_templates(template_directory)
 
     train_directory = os.path.join(generation_directory, "trains")
-    train_paths = io.list_trains(train_directory)
+    train_paths = list_trains(train_directory)
 
     message = "Different number of templates and trains between {} and {}".format(template_directory, train_directory)
     assert len(template_paths) == len(train_paths), message
 
     cells = {}
     for template_path, train_path in zip(template_paths, train_paths):
-        template = io.load_template(template_path)
-        train = io.load_train(train_path)
+        template = load_template(template_path)
+        train = load_train(train_path)
         raise NotImplementedError()  # TODO complete.
 
     raise NotImplementedError()  # TODO complete.
