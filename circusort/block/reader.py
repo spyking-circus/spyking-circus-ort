0e20a58a4094f426ef84480ea9ee666d87e7bf65
balefebvre <baptiste.lefebvre@ens.fr>
2017-12-11 11:06:01 +0100

    Cleaning the processing block 'reader'

--- circusort/block/reader.py
+++ circusort/block/reader.py
@@ -6,31 +6,66 @@ from circusort.io.generate import synthetic_grid
 
 
 class Reader(Block):
-    '''TODO add docstring'''
+    """Reader block.
+
+    Attributes:
+        data_path: string
+        dtype: string
+        repeat: boolean
+        nb_channels: integer
+        nb_samples: integer
+        sampling_rate:
+        data:
+        shape: tuple
+
+    See also:
+        circusort.block.Block
+    """
+    # TODO complete docstring.
 
     name = "File reader"
 
-    params = {'data_path'     : '/tmp/input.dat', 
-              'dtype'         : 'float32',
-              'repeat'        : False,
-              'nb_channels'   : 10,
-              'nb_samples'    : 1024, 
-              'sampling_rate' : 20000}
+    params = {
+        'data_path': "/tmp/input.dat",
+        'dtype': 'float32',
+        'repeat': False,
+        'nb_channels': 10,
+        'nb_samples': 1024,
+        'sampling_rate': 20000,
+    }
 
     def __init__(self, **kwargs):
+        """Initialization of the object.
+
+        Parameters:
+            data_path: string
+            dtype: string
+            repeat: boolean
+            nb_channels: integer
+            nb_samples: integer
+            sampling_rate:
+
+        See also:
+            circusort.block.Block
+        """
+        # TODO complete docstring.
 
         Block.__init__(self, **kwargs)
         self.add_output('data')
 
     def _initialize(self):
-        '''TODO add docstring'''
+        """Initialization of the processing block."""
+
         self.data  = numpy.memmap(self.data_path, dtype=self.dtype, mode='r')
         self.shape = (self.data.size/self.nb_channels, self.nb_channels)
         self.data  = None
         self.output.configure(dtype=self.dtype, shape=(self.nb_samples, self.nb_channels))
+
         return
 
     def _process(self):
+        """Process one buffer of data."""
+
         self.data = numpy.memmap(self.data_path, dtype=self.dtype, mode='r', shape=self.shape)
 
         i_min = self.nb_samples * self.counter
@@ -44,4 +79,5 @@ class Reader(Block):
           self.output.send(self.data[i_min:i_max, :])
   
         self.data = None
+
         return
