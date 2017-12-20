Fixing the computation of cross-correlogram

circusort/obj/train.py
@@ -10,10 +10,14 @@ from circusort.utils.path import normalize_path
 class Train(object):
     # TODO add docstring
 
-    def __init__(self, times):
+    def __init__(self, times, t_min=None, t_max=None):
         # TODO add docstring.
 
         self.times = times
+        self.times = self.times if t_min is None else self.times[t_min <= self.times]
+        self.times = self.times if t_max is None else self.times[self.times <= t_max]
+        self.t_min = min(0.0, np.min(times)) if t_min is None else t_min
+        self.t_max = np.max(times) if t_max is None else t_max
 
     @property
     def nb_times(self):
@@ -27,8 +31,8 @@ class Train(object):
         # TODO add docstring.
 
         # TODO improve method with two additional attributes: start_time and end_time.
-        times = np.max(self.times) + np.random.uniform(0.0, np.min(self.times)) - self.times
-        train = Train(times)
+        times = self.t_min + ((self.t_max - self.t_min) - (self.times - self.t_min))
+        train = Train(times, t_min=self.t_min, t_max=self.t_max)
 
         return train
 
@@ -36,11 +40,15 @@ class Train(object):
         # TODO add docstring.
 
         times = self.times
-        if isinstance(t_min, float):
+        if t_min is None:
+            t_min = self.t_min
+        elif isinstance(t_min, float):
             times = times[t_min <= times]
-        if isinstance(t_max, float):
+        if t_max is None:
+            t_max = self.t_max
+        elif isinstance(t_max, float):
             times = times[times <= t_max]
-        train = Train(times)
+        train = Train(times, t_min=t_min, t_max=t_max)
 
         return train
 
@@ -61,6 +69,9 @@ class Train(object):
     def _plot(self, ax, t_min=0.0, t_max=10.0, offset=0, **kwargs):
 
         _ = kwargs  # Discard additional keyword arguments.
+
+        t_min = self.t_min + t_min
+        t_max = self.t_min + t_max
 
         is_selected = np.logical_and(t_min <= self.times, self.times <= t_max)
         x = self.times[is_selected]

circusort/utils/train.py
@@ -1,54 +1,77 @@
 import numpy as np
 
 
-def compute_correlation(source_train, sink_train, bin_size=1e-3, lag_min=-100e-3, lag_max=+100e-3):
+def compute_correlation(source_train, sink_train, bin_size=1e-3, lag_min=-100e-3, lag_max=+100e-3, **kwargs):
     # TODO add docstring.
 
-    source_times = source_train.times
-    sink_times = sink_train.times
+    _ = kwargs  # Discard additional keyword arguments.
+
+    assert source_train.t_min == sink_train.t_min, "{} != {}".format(source_train.t_min, sink_train.t_min)
+    assert source_train.t_max == sink_train.t_max, "{} != {}".format(source_train.t_max, sink_train.t_max)
 
     n_min = int(np.ceil(lag_min / bin_size + 0.5))
     n_max = int(np.floor(lag_max / bin_size - 0.5))
     nb_bins = n_max - n_min + 1
-    lag = bin_size * np.linspace(n_min, n_max, num=nb_bins)
-    correlation = np.zeros(nb_bins)
+    lags = bin_size * np.linspace(n_min, n_max, num=nb_bins)
+    lag_min = bin_size * float(n_min - 0.5)
+    lag_max = bin_size * float(n_max + 0.5)
+    correlations = np.zeros(nb_bins)
 
-    if source_times.size == 0 or sink_times.size == 0:
+    if source_train.times.size == 0:
+        pass
+    elif sink_train.times.size == 0:
         pass
     else:
+        # Sort times.
+        source_times = np.sort(source_train.times)
+        sink_times = np.sort(sink_train.times)
+        # Iter through interesting pair of spikes.
         i, j = (0, 0)
         while i < source_times.size and j < sink_times.size:
             if source_times[i] < sink_times[j]:
                 k = i
-                while k < source_times.size and sink_times[j] - source_times[k] >= lag_max:
+                while k < source_times.size and lag_max <= sink_times[j] - source_times[k]:
                     k += 1
-                while k < source_times.size and sink_times[j] - source_times[k] >= lag_min:
+                while k < source_times.size and lag_min <= sink_times[j] - source_times[k]:
                     lag = sink_times[j] - source_times[k]
                     n = int(np.round(lag / bin_size))
-                    correlation[n] += 1.0
+                    correlations[n - n_min] += 1.0
                     k += 1
                 i += 1
             else:
                 k = j
                 while k < sink_times.size and sink_times[k] - source_times[i] < lag_min:
                     k += 1
-                while k < sink_times.size and sink_times[k] - source_times[i] < lag_min:
+                while k < sink_times.size and sink_times[k] - source_times[i] < lag_max:
                     lag = sink_times[k] - source_times[i]
                     n = int(np.round(lag / bin_size))
-                    correlation[n] += 1.0
+                    correlations[n - n_min] += 1.0
                     k += 1
                 j += 1
+        # Normalize correlations.
+        correlations /= (source_train.t_max - source_train.t_min)
 
-    correlation /= bin_size
-
-    return lag, correlation
+    return lags, correlations
 
 
-def compute_train_similarity(source_train, sink_train, **kwargs):
+def compute_reverted_correlation(source_train, sink_train, **kwargs):
+
+    assert source_train.t_min == sink_train.t_min
+    assert source_train.t_max == sink_train.t_max
+
+    reverted_sink_train = sink_train.reverse()
+    lags, correlations = compute_correlation(source_train, reverted_sink_train, **kwargs)
+
+    return lags, correlations
+
+
+def compute_train_similarity(source_train, sink_train, t_min=None, t_max=None, **kwargs):
     # TODO add docstring.
 
-    _, correlation = compute_correlation(source_train, sink_train, **kwargs)
-    similarity = np.max(correlation)
+    source_train = source_train.slice(t_min=t_min, t_max=t_max)
+    sink_train = sink_train.slice(t_min=t_min, t_max=t_max)
+    _, correlations = compute_correlation(source_train, sink_train, **kwargs)
+    similarity = np.max(correlations)
 
     return similarity
 
@@ -57,12 +80,12 @@ def compute_dip_strength(source_train, sink_train, lag_min=-100e-3, lag_max=+100
                          tau_min=-2.5e-3, tau_max=+2.5e-3, **kwargs):
     # TODO add docstring.
 
-    _, correlation = compute_correlation(source_train, sink_train,
+    _, correlations = compute_correlation(source_train, sink_train,
                                          lag_min=lag_min, lag_max=lag_max, **kwargs)
-    _, correlation_ = compute_correlation(source_train, sink_train.reverse(),
+    _, correlations_ = compute_correlation(source_train, sink_train.reverse(),
                                           lag_min=tau_min, lag_max=tau_max, **kwargs)
-    c = np.mean(correlation)
-    c_ = np.mean(correlation_)
+    c = np.mean(correlations)
+    c_ = np.mean(correlations_)
     strength = (c - c_) / (c + c_)
 
     return strength