spyking-circus/spyking-circus-ort

commit e3311d3532a0fd473c60399e2378e62c543590a9

Cleaning & WIP matching sorted units with generated units

circusort/__init__.py
@@ -6,7 +6,7 @@ from . import obj
 from . import plt
 
 from .base import create_director
-from .base import utils
+from .utils import *
 
 
 __version__ = '0.0.1'

circusort/base/__init__.py
@@ -1,6 +1,5 @@
 from .director import Director
 from .manager import Manager
-from .utils import find_interface_address_towards
 
 def create_director(host='127.0.0.1', **kwargs):
     '''Create a new director in this process.'''

circusort/base/director.py
@@ -1,10 +1,8 @@
 import time
 import logging
 
-from .logger import Logger
-from . import utils
-
-# from circusort.base.process import Process
+from circusort.base.logger import Logger
+from circusort.utils.base import get_log
 from circusort.base.process import create_process
 
 
@@ -18,7 +16,7 @@ class Director(object):
         self.log_level = log_level
         self.logger = Logger(interface=host)
         # Get logger instance
-        self.log = utils.get_log(self.logger.address, name=__name__, log_level=self.log_level)
+        self.log = get_log(self.logger.address, name=__name__, log_level=self.log_level)
 
         self.host = host
 

circusort/base/manager.py
@@ -1,6 +1,7 @@
-from circusort.base import utils
-from circusort.base.process import Process
 import logging
+
+from circusort.utils.base import get_log
+from circusort.base.process import Process
 
 
 class Manager(object):
@@ -20,7 +21,7 @@ class Manager(object):
         self.name = name or "Manager"
         if self.log_address is None:
             raise NotImplementedError("no logger address")
-        self.log = utils.get_log(self.log_address, name=__name__, log_level=self.log_level)
+        self.log = get_log(self.log_address, name=__name__, log_level=self.log_level)
         self.log.info("{d} is created".format(d=str(self)))
 
     def create_block(self, block_type, name=None, log_level=None, **kwargs):

circusort/base/process.py
@@ -5,7 +5,7 @@ import sys
 import zmq
 import logging
 
-from circusort.base import utils
+from circusort.utils.base import get_log, find_interface_address_towards
 from circusort.base.proxy import Proxy
 from circusort.base.serializer import Serializer
 
@@ -39,7 +39,7 @@ class Process(object):
             raise NotImplementedError()
             # TODO remove
         self.log_level = log_level
-        self.logger = utils.get_log(log_address, name=__name__, log_level=self.log_level)
+        self.logger = get_log(log_address, name=__name__, log_level=self.log_level)
 
         if host is None:
             self.host = '127.0.0.1'
@@ -55,7 +55,7 @@ class Process(object):
             self.logger.debug("create local process on {h}".format(h=self.host))
             # 1. Bind temporary socket
             transport = 'tcp'
-            localhost = utils.find_interface_address_towards(self.host)
+            localhost = find_interface_address_towards(self.host)
             port = '*'
             endpoint = '{h}:{p}'.format(h=localhost, p=port)
             address = '{t}://{e}'.format(t=transport, e=endpoint)
@@ -88,7 +88,7 @@ class Process(object):
             self.logger.debug("create remote process on {h}".format(h=self.host))
             # 1. Bind temporary socket
             transport = 'tcp'
-            localhost = utils.find_interface_address_towards(self.host)
+            localhost = find_interface_address_towards(self.host)
             port = '*'
             endpoint = '{h}:{p}'.format(h=localhost, p=port)
             address = '{t}://{e}'.format(t=transport, e=endpoint)

circusort/block/block.py
@@ -5,7 +5,7 @@ import logging
 import time
 
 from circusort.base.endpoint import Endpoint, EOCError
-from circusort.base import utils
+from circusort.utils.base import get_log
 
 
 class Block(threading.Thread):
@@ -30,7 +30,7 @@ class Block(threading.Thread):
         if self.log_address is None:
             raise NotImplementedError("no logger address")
 
-        self.log = utils.get_log(self.log_address, name=__name__, log_level=self.log_level)
+        self.log = get_log(self.log_address, name=__name__, log_level=self.log_level)
 
         self.parent = None
         self.host = None

circusort/cli/process.py
@@ -1,7 +1,7 @@
 import json
 import zmq
 
-from circusort.base import utils
+from circusort.utils.base import get_log
 from circusort.base.proxy import Proxy
 from circusort.block.block import Block
 
@@ -17,7 +17,7 @@ class Process(object):
         if log_address is None:
             raise NotImplementedError()
             # TODO remove
-        self.logger = utils.get_log(log_address, name=__name__)
+        self.logger = get_log(log_address, name=__name__)
 
         # TODO find proper space to define following class
         class Encoder(json.JSONEncoder):

circusort/utils/base.py
@@ -2,7 +2,7 @@ import array
 import fcntl
 import json
 import logging
-import random
+# import random
 import re
 import socket
 import struct
@@ -11,9 +11,27 @@ import time
 import zmq
 
 
+def append_hdf5(dataset, data):
+    """Append 1D-array to a HDF5 dataset.
+
+    Parameters:
+        dataset: ?
+            HDF5 dataset.
+        data: numpy.ndarray
+            1D-array.
+    """
+
+    old_size = len(dataset)
+    new_size = old_size + len(data)
+    new_shape = (new_size,)
+    dataset.resize(new_shape)
+    dataset[old_size:new_size] = data
+
+    return
+
 
 class LogHandler(logging.Handler):
-    '''Logging handler sending logging output to a ZMQ socket.'''
+    """Logging handler sending logging output to a ZMQ socket."""
 
     def __init__(self, address):
 
@@ -31,7 +49,7 @@ class LogHandler(logging.Handler):
 
         # TODO: make sure we are connected...
         #       (see http://stackoverflow.com/questions/23433037/sleep-after-zmq-connect#23437374)
-        duration = 0.1 # s
+        duration = 0.1  # s
         time.sleep(duration)
 
     def __del__(self):
@@ -41,11 +59,13 @@ class LogHandler(logging.Handler):
         super(LogHandler, self).close()
 
     def format(self, record):
-        '''Format a record.'''
+        """Format a record."""
+
         return self.formatter.format(record)
 
     def emit(self, record):
-        '''Emit a log message on the PUB socket.'''
+        """Emit a log message on the PUB socket."""
+
         topic = b'log'
         message = {
             'kind': 'log',
@@ -54,15 +74,18 @@ class LogHandler(logging.Handler):
         data = json.dumps(message)
         # data = data.encode('utf-8') # convert string to bytes
         self.socket.send_multipart([topic, data])
+
         return
 
     def handle(self, record):
+
         super(LogHandler, self).handle(record)
+
         return
 
 
 def get_log(address, name=None, log_level=logging.INFO):
-    '''Get a logger instance by name.'''
+    """Get a logger instance by name."""
 
     # Initialize the handler instance
     handler = LogHandler(address)
@@ -78,47 +101,55 @@ def get_log(address, name=None, log_level=logging.INFO):
     return logger
 
 
-def find_interfaces(format=True):
-    '''Enumerate all interfaces available on the system.
+def find_interfaces(format_=True):
+    """Enumerate all interfaces available on the system.
 
     Parameter
-    ---------
-    format: boolean
-        True: interfaces are returned as string
-        False: interfaces are returned as byte string
+        format_: boolean
+            True: interfaces are returned as string
+            False: interfaces are returned as byte string
 
     Code from http://code.activestate.com/recipes/439093/#c1
-    '''
+    """
+
     max_possible = 128  # arbitrary. raise if needed.
-    bytes = max_possible * 32
+    bytes_ = max_possible * 32
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    names = array.array('B', '\0' * bytes)
+    names = array.array('B', '\0' * bytes_)
     outbytes = struct.unpack('iL', fcntl.ioctl(
         s.fileno(),
         0x8912,  # SIOCGIFCONF
-        struct.pack('iL', bytes, names.buffer_info()[0])
+        struct.pack('iL', bytes_, names.buffer_info()[0])
     ))[0]
     namestr = names.tostring()
     lst = []
     for i in range(0, outbytes, 40):
         name = namestr[i:i+16].split('\0', 1)[0]
-        ip   = namestr[i+20:i+24]
+        ip = namestr[i+20:i+24]
         lst.append((name, ip))
-    if format:
+    if format_:
         lst = dict([(i[0], format_ip(i[1])) for i in lst])
+
     return lst
 
+
 def format_ip(addr):
+
     return str(ord(addr[0])) + '.' + \
            str(ord(addr[1])) + '.' + \
            str(ord(addr[2])) + '.' + \
            str(ord(addr[3]))
 
+
 def find_loopback_interface():
+
     interfaces = find_interfaces()
+
     return interfaces['lo']
 
+
 def find_ethernet_interface():
+
     interfaces = find_interfaces()
     if 'eth1' in interfaces:
         return interfaces['eth1']
@@ -129,16 +160,19 @@ def find_ethernet_interface():
 
 
 def extract_port(address):
+
     port = re.split(":", address)[-1]
+
     return port
 
+
 def find_interface_address_towards(host):
-    '''TODO add docstring'''
+    # TODO add docstring.
 
     p = subprocess.Popen(["ip", "route", "get", host],
                          stdout=subprocess.PIPE)
-    l = p.stdout.readlines()
-    s = l[0]
+    lines = p.stdout.readlines()
+    s = lines[0]
     k = 'address'
     p = "src (?P<{}>[\d,.]*)".format(k)
     m = re.compile(p)

commit 917b35347856fb1dcb75628e38375ee121ffe36d

Fixing remote import error introduced in e3311d3

circusort/__init__.py
@@ -6,7 +6,7 @@ from . import obj
 from . import plt
 
 from .base import create_director
-from .utils import *
+from .base import utils
 
 
 __version__ = '0.0.1'

circusort/base/__init__.py
@@ -1,8 +1,22 @@
 from .director import Director
 from .manager import Manager
+from .utils import find_interface_address_towards
+
 
 def create_director(host='127.0.0.1', **kwargs):
-    '''Create a new director in this process.'''
+    """Create a new director in this process.
+
+    Parameter:
+        host: string (optional)
+            The IP address of the host of the director.
+    Return:
+        director: circusort.base.director
+            The director.
+    See also:
+        circusort.base.director.Director
+    """
+
     interface = find_interface_address_towards(host)
     director = Director(interface, **kwargs)
+
     return director

circusort/base/director.py
@@ -2,7 +2,7 @@ import time
 import logging
 
 from circusort.base.logger import Logger
-from circusort.utils.base import get_log
+from circusort.base.utils import get_log
 from circusort.base.process import create_process
 
 

circusort/base/manager.py
@@ -1,6 +1,6 @@
 import logging
 
-from circusort.utils.base import get_log
+from circusort.base.utils import get_log
 from circusort.base.process import Process
 
 

circusort/base/process.py
@@ -5,10 +5,9 @@ import sys
 import zmq
 import logging
 
-from circusort.utils.base import get_log, find_interface_address_towards
+from circusort.base.utils import get_log, find_interface_address_towards
 from circusort.base.proxy import Proxy
 from circusort.base.serializer import Serializer
-
 
 
 def create_process(host=None, log_address=None, name=None, log_level=logging.INFO):

circusort/base/utils.py
@@ -1,0 +1,93 @@
+import json
+import logging
+import re
+import subprocess
+import time
+import zmq
+
+
+class LogHandler(logging.Handler):
+    """Logging handler sending logging output to a ZMQ socket."""
+
+    def __init__(self, address):
+
+        logging.Handler.__init__(self)
+
+        self.address = address
+
+        self.form = "%(levelname)s %(process)s %(name)s: %(message)s"
+        self.formatter = logging.Formatter(self.form)
+
+        # Set up logging connection
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.PUB)
+        self.socket.connect(self.address)
+
+        # TODO: make sure we are connected...
+        #       (see http://stackoverflow.com/questions/23433037/sleep-after-zmq-connect#23437374)
+        duration = 0.1  # s
+        time.sleep(duration)
+
+    def __del__(self):
+
+        self.socket.close()
+        self.context.term()
+        super(LogHandler, self).close()
+
+    def format(self, record):
+        """Format a record."""
+
+        return self.formatter.format(record)
+
+    def emit(self, record):
+        """Emit a log message on the PUB socket."""
+
+        topic = b'log'
+        message = {
+            'kind': 'log',
+            'record': record.__dict__,
+        }
+        data = json.dumps(message)
+        # data = data.encode('utf-8') # convert string to bytes
+        self.socket.send_multipart([topic, data])
+
+        return
+
+    def handle(self, record):
+
+        super(LogHandler, self).handle(record)
+
+        return
+
+
+def get_log(address, name=None, log_level=logging.INFO):
+    """Get a logger instance by name."""
+
+    # Initialize the handler instance
+    handler = LogHandler(address)
+
+    # Get a logger with the specified name (or the root logger)
+    logger = logging.getLogger(name=name)
+    # Set the threshold for this logger
+    logger.setLevel(log_level)
+    # logger.setLevel(logging.INFO)
+    # Add the specified handler to this logger
+    logger.addHandler(handler)
+
+    return logger
+
+
+def find_interface_address_towards(host):
+    # TODO add docstring.
+
+    p = subprocess.Popen(["ip", "route", "get", host],
+                         stdout=subprocess.PIPE)
+    lines = p.stdout.readlines()
+    s = lines[0]
+    k = 'address'
+    p = "src (?P<{}>[\d,.]*)".format(k)
+    m = re.compile(p)
+    r = m.search(s)
+    a = r.group(k)
+
+    return a

circusort/block/block.py
@@ -5,7 +5,7 @@ import logging
 import time
 
 from circusort.base.endpoint import Endpoint, EOCError
-from circusort.utils.base import get_log
+from circusort.base.utils import get_log
 
 
 class Block(threading.Thread):

circusort/cli/process.py
@@ -1,14 +1,14 @@
 import json
 import zmq
 
-from circusort.utils.base import get_log
+from circusort.base.utils import get_log
 from circusort.base.proxy import Proxy
 from circusort.block.block import Block
 
 
-
 class Process(object):
-    '''TODO add docstring'''
+    """Process object."""
+    # TODO add docstring.
 
     def __init__(self, host, address, log_address):
 

circusort/utils/base.py
@@ -30,77 +30,6 @@ def append_hdf5(dataset, data):
     return
 
 
-class LogHandler(logging.Handler):
-    """Logging handler sending logging output to a ZMQ socket."""
-
-    def __init__(self, address):
-
-        logging.Handler.__init__(self)
-
-        self.address = address
-
-        self.form = "%(levelname)s %(process)s %(name)s: %(message)s"
-        self.formatter = logging.Formatter(self.form)
-
-        # Set up logging connection
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.PUB)
-        self.socket.connect(self.address)
-
-        # TODO: make sure we are connected...
-        #       (see http://stackoverflow.com/questions/23433037/sleep-after-zmq-connect#23437374)
-        duration = 0.1  # s
-        time.sleep(duration)
-
-    def __del__(self):
-
-        self.socket.close()
-        self.context.term()
-        super(LogHandler, self).close()
-
-    def format(self, record):
-        """Format a record."""
-
-        return self.formatter.format(record)
-
-    def emit(self, record):
-        """Emit a log message on the PUB socket."""
-
-        topic = b'log'
-        message = {
-            'kind': 'log',
-            'record': record.__dict__,
-        }
-        data = json.dumps(message)
-        # data = data.encode('utf-8') # convert string to bytes
-        self.socket.send_multipart([topic, data])
-
-        return
-
-    def handle(self, record):
-
-        super(LogHandler, self).handle(record)
-
-        return
-
-
-def get_log(address, name=None, log_level=logging.INFO):
-    """Get a logger instance by name."""
-
-    # Initialize the handler instance
-    handler = LogHandler(address)
-
-    # Get a logger with the specified name (or the root logger)
-    logger = logging.getLogger(name=name)
-    # Set the threshold for this logger
-    logger.setLevel(log_level)
-    # logger.setLevel(logging.INFO)
-    # Add the specified handler to this logger
-    logger.addHandler(handler)
-
-    return logger
-
-
 def find_interfaces(format_=True):
     """Enumerate all interfaces available on the system.
 
@@ -164,19 +93,3 @@ def extract_port(address):
     port = re.split(":", address)[-1]
 
     return port
-
-
-def find_interface_address_towards(host):
-    # TODO add docstring.
-
-    p = subprocess.Popen(["ip", "route", "get", host],
-                         stdout=subprocess.PIPE)
-    lines = p.stdout.readlines()
-    s = lines[0]
-    k = 'address'
-    p = "src (?P<{}>[\d,.]*)".format(k)
-    m = re.compile(p)
-    r = m.search(s)
-    a = r.group(k)
-
-    return a