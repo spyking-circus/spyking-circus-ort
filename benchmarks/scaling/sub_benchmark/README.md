560504c93a4d63a418edd1ca15cdc5a830303710
balefebvre <baptiste.lefebvre@ens.fr>
2017-12-22 14:04:43 +0100

    Reformatting scaling sub-benchmark

--- benchmarks/scaling/sub_benchmark/README.md
+++ benchmarks/scaling/sub_benchmark/README.md
@@ -3,16 +3,23 @@ sub-network composed of a `reader` block and a `writer` block.
 
 To execute this benchmark use the following instructions:
 
-1. Launch IPython
+1. Change your current directory  
+`cd ~/circusort/benchmarks/scaling/sub_benchmark`
+1. Launch IPython  
 `$ ipython`
-2. Launch *(not available yet)*
+2. Launch *(not available yet)*  
 `In[1]: %run main.py`
 
 You can alternatively use one of the following instructions for step 2.:
 
-- Launch the generation *(not available yet)*
-`In[1]: %run main.py --generation-only`
-- Launch the sorting *(not available yet)*
-`In[1]: %run main.py --sorting-only`
-- Launch the introspection *(not available yet)*
-`In[1]: %run main.py --introspection-only`
+- Launch the generation only  
+*(not available yet)*  
+`In[2]: %run main.py --generation`
+- Launch the sorting only  
+(generation must have been executed before)  
+*(not available yet)*  
+`In[3]: %run main.py --sorting`
+- Launch the introspection only  
+(generation and sorting must have been executed before)  
+*(not available yet)*  
+`In[4]: %run main.py --introspection`
--- benchmarks/scaling/sub_benchmark/main.py
+++ benchmarks/scaling/sub_benchmark/main.py
@@ -6,56 +6,76 @@ import circusort
 from logging import DEBUG
 
 
-# TODO correct the following lines.
 # Parse command line.
 parser = argparse.ArgumentParser()
-parser.add_argument('--no-realism', dest='is_realistic', action='store_false', default=True)
-parser.add_argument('--no-sorting', dest='skip_sorting', action='store_true', default=False)
-parser.add_argument('--init-temp-dict', dest='init_temp_dict', action='store_true')
+parser.add_argument('--generation', dest='pending_generation', action='store_true', default=None)
+parser.add_argument('--sorting', dest='pending_sorting', action='store_true', default=None)
+parser.add_argument('--introspection', dest='pending_introspection', action='store_true', default=None)
 args = parser.parse_args()
+if args.pending_generation is None and args.pending_sorting is None and args.pending_introspection is None:
+    args.pending_generation = True
+    args.pending_sorting = True
+    args.pending_introspection = True
+else:
+    args.pending_generation = args.pending_generation == True
+    args.pending_sorting = args.pending_sorting == True
+    args.pending_introspection = args.pending_introspection == True
 
 
-# Define parameters
-host = '127.0.0.1'  # i.e. run the test locally
-nb_channels = 16
-nb_samples = 1024
-sampling_rate = 20e+3  # Hz
+# Define the working directory.
 directory = os.path.join("~", ".spyking-circus-ort", "benchmarks", "scaling_0")
 directory = os.path.expanduser(directory)
 if not os.path.isdir(directory):
-    message = "Directory does not exist: {}".format(directory)
-    raise OSError(message)
-sorting_directory = os.path.join(directory, "sorting")
-if not os.path.isdir(sorting_directory):
-    os.makedirs(sorting_directory)
-# TODO complete.
+    os.makedirs(directory)
 
 
-# Define keyword arguments.
-reader_kwargs = {
-    'data_path': os.path.join(directory, "data.raw"),
-    'dtype': 'int16',
-    'nb_channels': nb_channels,
-    'nb_samples': nb_samples,
-    'sampling_rate': sampling_rate,
-    'is_realistic': args.is_realistic,
-}
-signal_writer_kwargs = {
-    'data_path': os.path.join(sorting_directory, "data_filtered.raw"),
-}
+# Generate data (if necessary).
+if args.pending_generation:
+    
+    circusort.net.pregenerator(working_directory=directory)
 
 
-# Define the elements of the Circus network.
-director = circusort.create_director(host=host)
-manager = director.create_manager(host=host)
-reader = manager.create_block('reader', **reader_kwargs)
-writer = manager.create_block('writer', log_level=DEBUG, **signal_writer_kwargs)
-# Initialize the elements of the Circus network.
-director.initialize()
-# Connect the elements of the Circus network.
-director.connect(reader.output, reader.input)
-# or  # director.connect(reader.output, [reader.input])  # if the previous line does not work.
+# Sort data (if necessary).
+if args.pending_sorting:
+    
+    # Define parameters
+    host = '127.0.0.1'  # i.e. run the test locally
+    nb_channels = 16
+    nb_samples = 1024
+    sampling_rate = 20e+3  # Hz
+    sorting_directory = os.path.join(directory, "sorting")
+    if not os.path.isdir(sorting_directory):
+        os.makedirs(sorting_directory)
+    # TODO complete.
+    
+    
+    # Define keyword arguments.
+    reader_kwargs = {
+        'data_path': os.path.join(directory, "data.raw"),
+        'dtype': 'int16',
+        'nb_channels': nb_channels,
+        'nb_samples': nb_samples,
+        'sampling_rate': sampling_rate,
+        'is_realistic': args.is_realistic,
+    }
+    signal_writer_kwargs = {
+        'data_path': os.path.join(sorting_directory, "data_filtered.raw"),
+    }
+    
+    
+    # Define the elements of the Circus network.
+    director = circusort.create_director(host=host)
+    manager = director.create_manager(host=host)
+    reader = manager.create_block('reader', **reader_kwargs)
+    writer = manager.create_block('writer', log_level=DEBUG, **signal_writer_kwargs)
+    # Initialize the elements of the Circus network.
+    director.initialize()
+    # Connect the elements of the Circus network.
+    director.connect(reader.output, reader.input)
+    # or  # director.connect(reader.output, [reader.input])  # if the previous line does not work.
 
 
-# Analyze the real-time performances.
-# TODO complete.
+# Introspect sorting (if necessary).
+if args.pending_introspection:
+    
+    pass  # TODO complete.
