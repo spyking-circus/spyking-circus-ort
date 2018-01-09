81294422948d4c427913f040a523cb0d2aa75ccd
balefebvre <baptiste.lefebvre@ens.fr>
2018-01-09 16:30:35 +0100

    Adding expected outputs for the peak detection subnetwork

--- benchmarks/scaling/main.py
+++ benchmarks/scaling/main.py
@@ -86,57 +86,11 @@ def main():
 
             network.sorting(name)
 
-            # # TODO remove the following lines.
-            # # Load generation parameters.
-            # parameters = circusort.io.get_data_parameters(generation_directory)
-            #
-            # # Define parameters.
-            # host = '127.0.0.1'  # i.e. run the test locally
-            # dtype = parameters['general']['dtype']
-            # nb_channels = parameters['probe']['nb_channels']
-            # nb_samples = parameters['general']['buffer_width']
-            # sampling_rate = parameters['general']['sampling_rate']
-            #
-            # # Create directories (if necessary).
-            # if not os.path.isdir(sorting_directory):
-            #     os.makedirs(sorting_directory)
-            # if not os.path.isdir(introspection_directory):
-            #     os.makedirs(introspection_directory)
-            #
-            # # Define keyword arguments.
-            # reader_kwargs = {
-            #     'data_path': os.path.join(generation_directory, "data.raw"),
-            #     'dtype': dtype,
-            #     'nb_channels': nb_channels,
-            #     'nb_samples': nb_samples,
-            #     'sampling_rate': sampling_rate,
-            #     'is_realistic': True,
-            #     'introspection_path': introspection_directory,
-            # }
-            # signal_writer_kwargs = {
-            #     'data_path': os.path.join(sorting_directory, "data_filtered.raw"),
-            #     'introspection_path': introspection_directory,
-            # }
-            #
-            # # Define the elements of the Circus network.
-            # director = circusort.create_director(host=host)
-            # manager = director.create_manager(host=host)
-            # reader = manager.create_block('reader', **reader_kwargs)
-            # writer = manager.create_block('writer', log_level=DEBUG, **signal_writer_kwargs)
-            # # Initialize the elements of the Circus network.
-            # director.initialize()
-            # # Connect the elements of the Circus network.
-            # director.connect(reader.output, writer.input)
-            # # or  # director.connect(reader.output, [reader.input])  # if the previous line does not work.
-            # # Launch the network.
-            # director.start()
-            # director.join()
-            # director.destroy()
-
     # Introspect sorting (if necessary).
     if args.pending_introspection:
 
         block_names = network.block_names
+        showfliers = False
         speed_factors = OrderedDict()
         output_directory = os.path.join(directory, "output")
         if not os.path.isdir(output_directory):
@@ -187,7 +141,8 @@ def main():
             output_path = os.path.join(output_directory, output_filename)
 
             fig, ax = plt.subplots(1, 1, num=0, clear=True)
-            ax.boxplot(data, notch=True, whis=1.5, labels=block_names, flierprops=flierprops)
+            ax.boxplot(data, notch=True, whis=1.5, labels=block_names,
+                       flierprops=flierprops, showfliers=showfliers)
             ax.set_ylabel("speed factor")
             ax.set_title("Real-time performances ({} channels)".format(configuration_name))
             fig.tight_layout()
@@ -211,7 +166,8 @@ def main():
             output_path = os.path.join(output_directory, output_filename)
 
             fig, ax = plt.subplots(1, 1, num=0, clear=True)
-            ax.boxplot(data, notch=True, whis=1.5, labels=configuration_names, flierprops=flierprops)
+            ax.boxplot(data, notch=True, whis=1.5, labels=configuration_names,
+                       flierprops=flierprops, showfliers=showfliers)
             ax.set_xlabel("number of channels")
             ax.set_ylabel("speed factor")
             ax.set_title("Real-time performances ({})".format(block_name))
