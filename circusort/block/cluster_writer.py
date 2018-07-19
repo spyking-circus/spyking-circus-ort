86548db428c123de68810ceb848443d218974d4f
balefebvre <baptiste.lefebvre@ens.fr>
2018-07-19 12:27:13 +0200

    Update cluster writer & updater writer

--- circusort/block/cluster_writer.py
+++ circusort/block/cluster_writer.py
@@ -1,3 +1,5 @@
+import os
+import pickle
 import time
 
 from circusort.block.block import Block
@@ -10,14 +12,48 @@ class ClusterWriter(Block):
 
     name = "Cluster writer"
 
-    params = {}
+    params = {
+        'output_directory': None,
+    }
 
     def __init__(self, **kwargs):
 
         Block.__init__(self, **kwargs)
+
         self.add_input('templates', structure='dict')
 
+        # The following line is useful to disable a PyCharm warning.
+        self.output_directory = self.output_directory
+
+        self._output_counter = 0
+        self._sleep_duration = 0.01  # s
+
     def _initialize(self):
+
+        # Create output directory (if necessary).
+        if self.output_directory is not None and not os.path.isdir(self.output_directory):
+            os.makedirs(self.output_directory)
+            # Log debug message.
+            string = "{} creates output directory {}"
+            message = string.format(self.name_and_counter, self.output_directory)
+            self.log.debug(message)
+
+        return
+
+    def _save_templates(self, templates):
+
+        # Define output path.
+        output_filename = "templates_{}.pkl".format(self._output_counter)
+        output_path = os.path.join(self.output_directory, output_filename)
+        # Pickle template update.
+        with open(output_path, mode='wb') as output_file:
+            pickle.dump(templates, output_file)
+        # Update output counter.
+        self._output_counter += 1
+        # Log debug message.
+        string = "{} saves templates in {}"
+        message = string.format(self.name_and_counter, output_path)
+        self.log.debug(message)
 
         return
 
@@ -28,17 +64,18 @@ class ClusterWriter(Block):
         self._measure_time(label='start', frequency=10)
 
         if templates_packet is None:
-            duration = 0.01  # s
-            time.sleep(duration)
+            # Wait before entering next loop.
+            time.sleep(self._sleep_duration)
         else:
-            templates = templates_packet['payload']
-            # TODO remove the 3 following lines.
-            string = "{} receives templates: {}"
-            message = string.format(self.name, templates)
+            # Log debug message.
+            string = "{} receives templates"
+            message = string.format(self.name_and_counter)
             self.log.debug(message)
-            # TODO remove the 2 following lines.
-            duration = 0.01  # s
-            time.sleep(duration)
+            # Extract templates.
+            templates = templates_packet['payload']
+            # Save templates.
+            if self.output_directory is not None:
+                self._save_templates(templates)
 
         self._measure_time(label='end', frequency=10)
 
--- circusort/block/updater_writer.py
+++ circusort/block/updater_writer.py
@@ -1,3 +1,5 @@
+import os
+import pickle
 import time
 
 from circusort.block.block import Block
@@ -10,14 +12,48 @@ class UpdaterWriter(Block):
 
     name = "Updater writer"
 
-    params = {}
+    params = {
+        'output_directory': None,
+    }
 
     def __init__(self, **kwargs):
 
         Block.__init__(self, **kwargs)
+
         self.add_input('updater', structure='dict')
 
+        # The following line is useful to disable a PyCharm warning.
+        self.output_directory = self.output_directory
+
+        self._output_counter = 0
+        self._sleep_duration = 0.01  # s
+
     def _initialize(self):
+
+        # Create output directory if necessary.
+        if self.output_directory is not None and not os.path.isdir(self.output_directory):
+            os.makedirs(self.output_directory)
+            # Log debug message.
+            string = "{} creates output directory {}"
+            message = string.format(self.name_and_counter, self.output_directory)
+            self.log.debug(message)
+
+        return
+
+    def _save_update(self, update):
+
+        # Define output file.
+        output_filename = "template_update_{}.pkl".format(self._output_counter)
+        output_path = os.path.join(self.output_directory, output_filename)
+        # Pickle template update.
+        with open(output_path, mode='wb') as output_file:
+            pickle.dump(update, output_file)
+        # Update output counter.
+        self._output_counter += 1
+        # Log debug message.
+        string = "{} saves template update in {}"
+        message = string.format(self.name_and_counter, output_path)
+        self.log.debug(message)
 
         return
 
@@ -28,17 +64,18 @@ class UpdaterWriter(Block):
         self._measure_time(label='start', frequency=10)
 
         if updater_packet is None:
-            duration = 0.01  # s
-            time.sleep(duration)
+            # Wait before entering next loop.
+            time.sleep(self._sleep_duration)
         else:
-            updater = updater_packet['payload']
-            # TODO remove the 3 following lines.
-            string = "{} receives updater: {}"
-            message = string.format(self.name, updater)
+            # Log debug message.
+            string = "{} receive updater"
+            message = string.format(self.name_and_counter)
             self.log.debug(message)
-            # TODO remove the 2 following lines.
-            duration = 0.01  # s
-            time.sleep(duration)
+            # Extract template update.
+            update = updater_packet['payload']
+            # Save template update.
+            if self.output_directory is not None:
+                self._save_update(update)
 
         self._measure_time(label='end', frequency=10)
 
