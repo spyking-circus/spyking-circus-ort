f60189e696d5b4567f57238dae085c9471d7cde9
IDV User <pierre.yger@gmail.com>
2019-03-29 11:18:54 +0100

    utf8 encoding

--- circusort/block/density_clustering.py
+++ circusort/block/density_clustering.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from .block import Block
 import numpy as np
 import os
--- circusort/block/pca.py
+++ circusort/block/pca.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from .block import Block
 
 # import matplotlib.pyplot as plt
