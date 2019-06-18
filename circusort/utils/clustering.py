4b4be776a81847e732ec8bf68ef54a5db83ac6b8
yger <pierre.yger@gmail.com>
2019-06-18 16:02:15 +0200

    Patch

--- circusort/utils/clustering.py
+++ circusort/utils/clustering.py
@@ -788,7 +788,10 @@ class OnlineManager(object):
 
         for i in range(N):
             data = distances.get_row(i, with_diag=False)
-            dist_sorted[i] = data[np.argpartition(data, nb_neighbors)[:nb_neighbors]]
+            if len(data) > nb_neighbors:
+                dist_sorted[i] = data[np.argpartition(data, nb_neighbors)[:nb_neighbors]]
+            else:
+                dist_sorted[i] = data
             rho[i] = np.mean(dist_sorted[i])
 
         rho = -rho + rho.max()
